e3cd363c3ca4386abce0c797e7b62b23580c4e55
luckyAkbar <luckyakbar1509@gmail.com>
2021-12-28 20:13:11 +0700

    add new route to /agenda, /asetDesa, /infoDemografis, /infoGeografis

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,10 +1,28 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router, Request, Response } from 'express';
+import homeController from '../controller/controller.home';
+import agendaController from '../controller/controller.agenda';
+import asetDesaController from '../controller/controller.asetDesa';
+import infoDemografisController from '../controller/controller.infoDemografis';
+import infoGeografisController from '../controller/controller.infoGeografis';
 
 const router: Router = express.Router();
 
 router.route('/')
+  .get(homeController)
   .post((req: Request, res: Response): void => {
     res.status(200).json({ message: req.body });
   });
 
+router.route('/agenda')
+  .get(agendaController);
+  
+router.route('/asetDesa')
+  .get(asetDesaController);
+
+router.route('/infoDemografis')
+  .get(infoDemografisController);
+
+router.route('/infoGeografis')
+  .get(infoGeografisController);
+  
 export default router;

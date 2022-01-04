78cd667d49162822068e1077a787f95cc35e4160
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-04 19:23:18 +0700

    fix unnecessary call to mongoose.connect

--- src/class/BasicPage.ts
+++ src/class/BasicPage.ts
@@ -1,13 +1,11 @@
 import { strict as assert } from 'assert';
 import { FooterLinkContent } from '../interface/pageResource/footer';
 import FooterLinkModel from '../models/frontend/footerLink';
-import mongoConnect from '../connection/mongoConnect';
 import CustomError from './CustomError';
 
 class BasicPage {
   static async getFooterLinks(): Promise<Array<FooterLinkContent>> {
     try {
-      await mongoConnect();
       const result = await FooterLinkModel.findOne({});
       assert.notStrictEqual(result, null, new CustomError(
         'Important data is missing from database. This will result in user to see blank footer',

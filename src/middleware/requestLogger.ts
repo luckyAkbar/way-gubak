6744ccae00231ea3bcf82dfa08c78e7a45e391d0
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-04 19:19:15 +0700

    change request logger from using process.stdout to using fs.createWriteStream

--- src/middleware/requestLogger.ts
+++ src/middleware/requestLogger.ts
@@ -1,17 +1,32 @@
 import dotenv from 'dotenv';
+
+import fs from 'fs';
+import path from 'path';
+import { Request } from 'express';
+import morgan from 'morgan';
+import ServerConfigError from '../class/ServerConfigError';
+
 dotenv.config();
 
-import { Request, Response } from 'express';
-import morgan from 'morgan';
+const getRequestLogPath = (): string => {
+  const requestLogPath: string | undefined = process.env.REQUEST_LOG_PATH;
+
+  if (requestLogPath === undefined) throw new ServerConfigError('REQUEST_LOG_PATH is undefined. Please specify one in your .env file');
+
+  return requestLogPath;
+};
 
 const requestLogger = morgan('short', {
-  skip: function(req: Request, res: Response): boolean {
+  stream: fs.createWriteStream(path.join(getRequestLogPath(), 'request.log'), {
+    flags: 'a',
+  }),
+  skip(req: Request): boolean {
     const { path } = req;
     const skippedReqLoggingRoutes: string[] | undefined = process.env.SKIPPED_REQ_LOGGING_PATHS?.split(',');
 
     if (skippedReqLoggingRoutes === undefined) return false;
     return (skippedReqLoggingRoutes.includes(path));
-  }
+  },
 });
 
 export default requestLogger;

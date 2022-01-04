0018c35220fe9686cd2a836c5720024fdd5fd83a
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-04 11:13:45 +0700

    add new data to footer

--- src/interface/pageRenderingData/basicPageData.ts
+++ src/interface/pageRenderingData/basicPageData.ts
@@ -2,6 +2,10 @@ interface BasicPageData {
   villageName: string | undefined,
   districtName: string | undefined,
   copyrightYear: string | undefined,
+  provinceName: string | undefined,
+  phoneContact: string | undefined,
+  villageEmailAddress: string | undefined,
+  postalCode: string | undefined,
 };
 
 export default BasicPageData;

ab6a1f2e46491db86ba7b528176e12b8d36f4f35
luckyAkbar <luckyakbar1509@gmail.com>
2022-01-26 09:35:19 +0700

    initial commit for new static class method

--- src/class/ProductUMKM.ts
+++ src/class/ProductUMKM.ts
@@ -118,4 +118,170 @@ export default class ProductUMKM {
       err.logError();
     }
   }
+
+  static async getLatestProducts(): Promise<Array<IProduct>> {
+    try {
+      const latestProducts = await Product.find().sort({
+        createdAt: -1,
+      });
+
+      assert.notStrictEqual(latestProducts.length, 0);
+
+      return latestProducts;
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to fetch latest product. this may result user seeing empty product.', (e as Error).message);
+      await err.logError();
+
+      return [];
+    }
+  }
+
+  static async getFeaturedProduct(): Promise<Array<FeaturedProductItem[]>> {
+    try {
+      const featuredProductNames = await ProfileUMKM.find().select({
+        featuredImageName: 1,
+        _id: 0,
+      });
+
+      const featuredProductData = await Product.find({
+        imageName: {
+          $in: featuredProductNames.map(({ featuredImageName }) => featuredImageName),
+        },
+      });
+
+      const shapedProductData: Array<FeaturedProductItem[]> = featuredProductDataShaper(featuredProductData);
+
+      return shapedProductData;
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to get featured product. This may result user not seeing our featured product.', (e as Error).message);
+      await err.logError();
+
+      throw err;
+    }
+  }
+
+  static async getMostLikedProduct(numProduct: number = 5): Promise<Array<IProduct>> {
+    try {
+      const mostLikedProduct = await Product.find().sort({
+        'totalLikes': -1,
+      }).limit(numProduct);
+
+      assert.notStrictEqual(mostLikedProduct.length, 0);
+
+      return mostLikedProduct;
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to fetch most liked product. This may result in user seeing empty product.', (e as Error).message);
+      await err.logError();
+
+      return [];
+    }
+  }
+
+  static async getMostExpensiveProduct(numProducts: number = 5): Promise<Array<IProduct>> {
+    try {
+      const mostExpensiveProduct = await Product.find().sort({
+        'price': -1,
+      }).limit(numProducts);
+
+      assert.notStrictEqual(mostExpensiveProduct.length, 0);
+
+      return mostExpensiveProduct;
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to fetch most expensive product. This may result in user seeing empty product.', (e as Error).message);
+      await err.logError();
+
+      return [];
+    }
+  }
+
+  static async getCarouselItems(): Promise<Array<CarouselItem>> {
+    const carouselItems: CarouselItem[] = [];
+    const productHrefPrefix = process.env.PREFIX_PRODUCT_LINK ? process.env.PREFIX_PRODUCT_LINK : '/umkm/product/';
+    const imageLinkPrefix = process.env.PREFIX_IMAGE_HREF_LINK ? process.env.PREFIX_IMAGE_HREF_LINK : '/images/';
+
+    try {
+      const mostExpensiveProduct = await this.getMostExpensiveProduct();
+
+      mostExpensiveProduct.forEach((product) => {
+        carouselItems.push({
+          imageAlt: product.imageAlt,
+          productName: product.name,
+          productDesc: product.descriptions.join(' '),
+          productLink: `${productHrefPrefix}${product.id}`,
+          imageLink: `${imageLinkPrefix}${product.imageName}`,
+        });
+      });
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to fetch carousel items. this may result user seeing empty carousel in product index page', (e as Error).message);
+      await err.logError();
+    } finally {
+      return carouselItems;
+    }
+  }
+
+  static async getSidebarProducts(): Promise<Array<SidebarItem>> {
+    const sidebarProducts: SidebarItem[] = [];
+    const productHrefPrefix = process.env.PREFIX_PRODUCT_LINK ? process.env.PREFIX_PRODUCT_LINK : '/umkm/product/';
+    const imageLinkPrefix = process.env.PREFIX_IMAGE_HREF_LINK ? process.env.PREFIX_IMAGE_HREF_LINK : '/images/';
+
+    try {
+      const mostLikedProducts = await this.getMostLikedProduct(4);
+
+      mostLikedProducts.forEach((product) => {
+        sidebarProducts.push({
+          currency: product.currency,
+          price: product.price,
+          imageLink: `${imageLinkPrefix}${product.imageName}`,
+          productName: product.name,
+          productLink: `${productHrefPrefix}${product.id}`,
+        });
+      });
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to generate sidebar items. this may result user seeing empty product in sidebar', (e as Error).message);
+      await err.logError();
+    } finally {
+      return sidebarProducts;
+    }
+  }
+
+  static async getLatestProductPageData(): Promise<Array<LatestProductItem>> {
+    const latestProductPageData: LatestProductItem[] = [];
+    try {
+      const latestProducts = await this.getLatestProducts();
+
+      latestProducts.forEach((product) => {
+        latestProductPageData.push({
+          href: `${ProductUMKM.staticProductHrefPrefix}${product.id}`,
+          name: product.name,
+          shortDesc: product.descriptions.join(' ').substring(0, 35).concat('...'),
+          currency: product.currency,
+          price: product.price,
+          imageLink: `${ProductUMKM.staticImageLinkPrefix}${product.imageName}`
+        })
+      });
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to fetch latest product. this may result user seeing empty product.', (e as Error).message);
+      await err.logError();
+    } finally {
+      return latestProductPageData;
+    }
+  }
+
+  static async getProductIndexPageData(): Promise<ProductIndexPageData> {
+    try {
+      const productIndexPageData: ProductIndexPageData = {
+        carouselItems: await this.getCarouselItems(),
+        sidebarItems: await this.getSidebarProducts(),
+        featuredProducts: await this.getFeaturedProduct(),
+        latestProducts: await this.getLatestProductPageData(),
+      };
+
+      return productIndexPageData;
+    } catch (e: unknown) {
+      const err = new CustomError('System failed to fetch product index page data. this may result user seeing invalid product index page.', (e as Error).message, );
+      await err.logError();
+
+      throw err;
+    }
+  }
 }
